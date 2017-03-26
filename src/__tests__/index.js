899c0e8103f77d684d24b8e5f8aa89bca161dbfe
chenliang <kidliaa@gmail.com>
2017-03-26 20:39:04 +0800

    Use Jest as default test runner.

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
-
 import {shallow, mount, render} from 'enzyme';
-import {expect} from 'chai';
 import sinon from 'sinon';
 
 import MyComponent from '../index';
@@ -14,16 +12,16 @@ describe('Shallow Rendering', () => {
 
     it('to have three `.icon-test`s', () => {
         const wrapper = shallow(<MyComponent />);
-        expect(wrapper.find('.icon-test')).to.have.length(3);
+        expect(wrapper.find('.icon-test').length).toBe(3)
     });
 
     it('simulates click events', () => {
-        const buttonClick = sinon.spy();
+        const buttonClick = jest.fn();
         const wrapper = shallow(
           <MyComponent handleClick={buttonClick} />
         );
         wrapper.find('button').simulate('click');
-        expect(buttonClick.calledOnce).to.equal(true);
+        expect(buttonClick).toBeCalled();
     });
 
 });
@@ -34,15 +32,13 @@ describe('Full DOM Rendering', () => {
 
     it('allows us to set props', () => {
         const wrapper = mount(<MyComponent bar='baz' />);
-        expect(wrapper.props().bar).to.equal('baz');
-        wrapper.setProps({ bar: 'foo' });
-        expect(wrapper.props().bar).to.equal('foo');
+        expect(wrapper.props().bar).toBe('baz');
     });
 
     it('calls componentDidMount', () => {
         sinon.spy(MyComponent.prototype, 'componentDidMount');
         const wrapper = mount(<MyComponent />);
-        expect(MyComponent.prototype.componentDidMount.calledOnce).to.be.true;
+        expect(MyComponent.prototype.componentDidMount.calledOnce).toBe(true);
         MyComponent.prototype.componentDidMount.restore();
     });
 
@@ -54,7 +50,7 @@ describe('Static Rendered Markup', () => {
 
     it('renders three `.icon-test`s', () => {
         const wrapper = render(<MyComponent />);
-        expect(wrapper.find('.icon-test').length).to.equal(3);
+        expect(wrapper.find('.icon-test').length).toBe(3);
     });
 
 });
